cd5e2a5a8ca3415f3a3c6317f256a4e413786db4
dc0dr <ecdaniels826@outlook.com>
2021-10-21 23:32:51 +0000

    Started the Login Screen...

--- screens/Login.js
+++ screens/Login.js
@@ -1,1 +1,43 @@
-import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity ,View, TextInput} from 'react-native';
+
+const Login = () =>{
+    return(
+        <View style = {loginStyles.containerView}>
+            <Text> Welcome to my store! </Text>
+
+            <Text> Email </Text> 
+            
+            <TextInput
+                style = {loginStyles.textInput}
+                placeholder="Enter Email"
+            />
+            <TextInput 
+                style = {loginStyles.textInput}
+                placeholder="Enter password"
+                />
+
+        </View>
+    );
+}
+
+export default Login;
+
+const loginStyles = StyleSheet.create({
+    containerView: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textInput: {
+        height: 37,
+        width: 200,
+        margin: 12,
+        borderWidth: 1,
+        padding: 12,
+        borderRadius: 5,
+    }
+
+});
