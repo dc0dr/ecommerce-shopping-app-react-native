8b7c412c63e0ecf1307d0e7e3d963b490c5e9446
dc0dr <ecdaniels826@outlook.com>
2021-11-02 05:41:26 +0000

    Worked on the Home page for the most part (20% complete)

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -4,7 +4,34 @@ import { FontAwesome, Ionicons } from '@expo/vector-icons';
 
 
 
-const HomePage = () => {
+const HomePage = (params) => {
+    const [shopItems, setItems] = React.useState(0);
+
+    async function getItems() {
+      fetch(
+        'https://fakestoreapi.com/products/1'
+      )
+      .then(res => res.json())
+      .then(json => {
+      setItems(json);
+      console.log(json);
+    })
+      .catch((e) => {
+        console.log(e);
+      })
+    }
+
+    const DATA = [
+      'Card 1',
+      'Card 2',
+      'Card 3',
+      'Card 4',
+    ]
+
+    React.useEffect(() => {
+      getItems();
+    }, []);
+
     return(
       <View style={styles.container}>
         <View style={{
@@ -17,25 +44,42 @@ const HomePage = () => {
             style = {{
               borderRadius: 8, fontSize: 15, marginBottom: 20, 
               width: 200, textAlign: 'justify', backgroundColor: '#e6e4e1',
-              padding: 5}}
+              padding: 6, marginTop: 7, marginLeft: 8}}
             placeholder = "Enter search here "
           />
 
           <View style={{
-            backgroundColor: '#e6e4e1', marginBottom: 20, borderRadius: 8, justifyContent: 'center',
-            paddingHorizontal: 6, paddingVertical: 6 }}>
-            <FontAwesome name='bell' size={24} color='black'/>
+            marginBottom: 20, borderRadius: 8, alignSelf: 'center',
+            padding: 5, marginTop: 3, marginRight: 5}}>
+            <FontAwesome.Button name='bell' size={24} color='grey' backgroundColor='#e6e4e1' onPress={() => alert('nothing')}>
+            </FontAwesome.Button>
           </View>
         </View>
             
         <View style={{
-          flex: 0.19, backgroundColor: '#888', borderRadius: 10, alignItems: 'center', justifyContent: 'center'}}>
+          flex: 0.19, backgroundColor: '#ffa18a', borderRadius: 10, alignItems: 'center', 
+          justifyContent: 'center', marginLeft: 6, marginRight: 6}}>
           <Text> Second view </Text>
         </View>
 
         <View style={{
-          flex: 0.68, marginTop: 30, backgroundColor: '#888', alignItems: 'center', justifyContent: 'center'}}>
-          <Text> Third view.. </Text>
+          flex: 0.77, marginTop: 30, backgroundColor: '#ffa18a', 
+          alignItems: 'flex-start', justifyContent: 'flex-start',
+          marginLeft: 6, marginRight: 6, borderRadius: 8 }}>
+          <FlatList
+            data = {DATA}
+            renderItem = {({item}) =>
+              <View style={{
+                padding: 15, height: 220, width: 150,
+                backgroundColor: '#fff', marginBottom: 10, marginTop: 5, 
+                borderRadius: 8, marginLeft: 13, marginRight: 10
+                }}>
+                <Text> {item} </Text>
+              </View>
+            }
+            horizontal={false}
+            numColumns={2}
+          />
         </View>
       </View>
     );
