5058aca575ec1b9d68ad5258329acc64e106c68c
dc0dr <ecdaniels826@outlook.com>
2021-11-16 00:38:39 +0000

    Added the products (merch)

---
+++ src/merch/merch.js
@@ -0,0 +1,39 @@
+const merch = [{
+    name : "Demon Slayer - Rengoku Pop-up Parade",
+    image : require('../merchImages/demon-slayer-rengoku-nendroid-1.jpg'),
+    description : 'The Flame Hashira. One with a spirit that burns hotter and prouder than any other.',
+    price: '90'
+},
+{
+    name: "Demon Slayer - Giyu Tomioka Scale Figure",
+    image: require('../merchImages/demon-slayer-giyu-nendroid-1.jpg'),
+    description: 'The user of Water Breathing, Giyu Tomioka, will be released by Kotobukiya as a 1/8 scale figure! From the anime, Demon Slayer: Kimetsu no Yaiba, the Water Hashira Giyu Tomioka joins Kotobukiya’s line of scale figures. The lightly fluttering haori and the wildly rippling waters of the base bring to life the very moment when Giyu’s enemy is cut down before his eyes. The pattern of the distinctive haori and the words "Demon Extermination" (悪鬼滅殺) on the Nichirin Blade have been reproduced in great detail.',
+    price: '129'
+},
+{
+    name: "",
+    image: require('../merchImages/one-piece-keychain.jpg'),
+    description: "",
+    price: "15"
+},
+{
+    name: "",
+    image: require('../merchImages/sasuke-nendroid-1.jpg'),
+    description: "",
+    price: "120"
+},
+{
+    name: "",
+    image: require('../merchImages/sasuke-keychain.jpg'),
+    description: "",
+    price: "18"
+},
+{
+    name: "",
+    image: require('../merchImages/naruto-ninja-war-nendroid-1.jpg'),
+    description: "",
+    price: "300"
+}
+];
+
+export default merch;
