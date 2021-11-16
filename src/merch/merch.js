48d3e13d6ee83e0176b56a544efbef9dc8f8b3b2
dc0dr <ecdaniels826@outlook.com>
2021-11-16 22:34:18 +0000

    Added descriptions for some of the items

--- src/merch/merch.js
+++ src/merch/merch.js
@@ -7,31 +7,32 @@ const merch = [{
 {
     name: "Demon Slayer - Giyu Tomioka Scale Figure",
     image: require('../merchImages/demon-slayer-giyu-nendroid-1.jpg'),
+    image1: require('../merchImages/demon-slayer-giyuu-nendroid-2.jpg'),
     description: 'The user of Water Breathing, Giyu Tomioka, will be released by Kotobukiya as a 1/8 scale figure! From the anime, Demon Slayer: Kimetsu no Yaiba, the Water Hashira Giyu Tomioka joins Kotobukiya’s line of scale figures. The lightly fluttering haori and the wildly rippling waters of the base bring to life the very moment when Giyu’s enemy is cut down before his eyes. The pattern of the distinctive haori and the words "Demon Extermination" (悪鬼滅殺) on the Nichirin Blade have been reproduced in great detail.',
     price: '129'
 },
 {
-    name: "",
+    name: "One Piece - Zoro, Luffy, Sanji Metal Keychain",
     image: require('../merchImages/one-piece-keychain.jpg'),
-    description: "",
+    description: "Officially Licensed, Material: Metal",
     price: "15"
 },
 {
-    name: "",
+    name: "Naruto - Uchiha Sasuke Nendroid Figurine",
     image: require('../merchImages/sasuke-nendroid-1.jpg'),
     description: "",
     price: "120"
 },
 {
-    name: "",
+    name: "Naruto - Uchiha Sasuke Metal Keychain",
     image: require('../merchImages/sasuke-keychain.jpg'),
     description: "",
     price: "18"
 },
 {
-    name: "",
+    name: "Naruto - Naruto Uzumaki G.E.M Figurine",
     image: require('../merchImages/naruto-ninja-war-nendroid-1.jpg'),
-    description: "",
+    description: "It's been over a year since Naruto Uzumaki last joined the G.E.M. series, but now he is joining once again this time based on his battle-ready appearance from the Fourth Shinobi World War!. Falling down with the wind in his hair, he performs hand seals creating a dynamic pose that captures the feeling of wind beautifully. The chain mail part of his outfit makes use of a netted material for a more realistic texture.",
     price: "300"
 }
 ];
