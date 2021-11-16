e0f1bee59d2c24f7bcde39a7aad8a0e96365daff
dc0dr <ecdaniels826@outlook.com>
2021-11-16 22:35:15 +0000

    Added some Text and TextInput for the Checkout screen

--- src/screens/Checkout.js
+++ src/screens/Checkout.js
@@ -1,9 +1,34 @@
 import * as React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, TextInput, StyleSheet} from 'react-native';
 
-const Checkout = () => {
-
+const Checkout = ({navigation}) => {
     <View>
+        <View style={styles.inputContainer}>
+            <Text> Enter your shipping information below: </Text>
+        </View>
+        <View style={styles.inputContainer}>
+            <Text> Full name: </Text>
+            <TextInput />
+        </View>
 
+        <View style={styles.inputContainer}>
+            <Text> Address: </Text>
+            <TextInput />
+        </View>
+
+        <View style={styles.inputContainer}>
+            <Text> Email: </Text>
+            <TextInput />
+        </View>
     </View>
 }
+
+export default Checkout;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        flexDirection: 'row',
+        marginRight: 20
+    }
+});
+
