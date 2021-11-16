c92dd714854cf7de558f1a61949ee9d8d1645513
dc0dr <ecdaniels826@outlook.com>
2021-11-16 22:33:35 +0000

    Removed the header from the ProductDetails screen

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import * as React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity ,View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from  '@react-navigation/native-stack';
-import Login from './screens/Login';
-import HomePage from './screens/HomePage';
-import ProductDetails from './screens/ProductDetails';
-import Cart from './screens/Cart';
-import Checkout from './screens/Checkout';
-import Settings from './screens/Settings';
+import Login from './src/screens/Login.js';
+import HomePage from './src/screens/HomePage.js';
+import ProductDetails from './src/screens/ProductDetails.js';
+import Cart from './src/screens/Cart.js';
+import Checkout from './src/screens/Checkout';
+import Settings from './src/screens/Settings.js';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,15 +17,18 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen 
-        name = 'Login'
-        component = {Login}/>
-        <Stack.Screen
+        // name = 'Login'
+        // component = {Login}/>
+        // <Stack.Screen
         name = 'Home'
         component = {HomePage}/>
         <Stack.Screen
         name = 'Settings'
         component = {Settings}/>
         <Stack.Screen
+        options={{
+          headerShown: false,
+        }}
         name = 'Product'
         component = {ProductDetails}/>
         <Stack.Screen
